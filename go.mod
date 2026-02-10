77512c5203040be1d9461ae0cb50b10ce96b0898
Dillon <dfcross18@gmail.com>
2026-02-09 19:44:14 -0700

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module event_processing_pipeline
+
+go 1.22.2
---
+++ main.go
@@ -0,0 +1,12 @@
+
+package main
+
+import (
+	"fmt"
+	"event_processing_pipeline/api"
+)
+
+func main() {
+	fmt.Println("main.go running")
+	api.Run()
+}
