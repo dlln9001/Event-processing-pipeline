035960d8c5a5a9a0b596aeed3df2abcc39c0819e
Dillon <dfcross18@gmail.com>
2026-02-27 12:47:07 -0700

    added initial kafka listener

--- worker/src/main/java/com/event_processing/worker/transactions/TransactionsConsumer.java
+++ worker/src/main/java/com/event_processing/worker/transactions/TransactionsConsumer.java
@@ -1,5 +1,13 @@
 package com.event_processing.worker.transactions;
 
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
 public class TransactionsConsumer {
 
+    @KafkaListener(id = "transaction-group", topics = "topic-A")
+    public void listen(String message) {
+        System.out.println("Received message: " + message);
+    }
 }
