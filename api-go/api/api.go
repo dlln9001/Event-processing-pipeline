0b97272d7a098326dbbf48344dc158df84a0019d
Dillon <dfcross18@gmail.com>
2026-02-28 09:52:32 -0700

    binded router to a new port

--- api-go/api/api.go
+++ api-go/api/api.go
@@ -25,44 +25,46 @@ func Run() {
 	defer w.Close()
 
 	r := gin.Default()
-
+	
 	r.GET("/ping", func(ctx *gin.Context) {
 		ctx.JSON(http.StatusOK, "hello")
 	})
-
+	
+	
+	// makes a transaction object and sends a transaction event to kafka 
 	r.POST("/transaction", func(ctx *gin.Context) {
 		var req models.Transaction
 		err := ctx.ShouldBindJSON(&req)
-
+		
 		if err != nil {
 			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 			return
 		}
-
+		
 		payload, err := json.Marshal(req)
-
+		
 		if err != nil {
 			ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to encode JSON"})
 			return
 		}
-
+		
 		err = w.WriteMessages(context.Background(),
-			kafka.Message{
-				Key:   []byte("Key-A"),
-				Value: []byte(payload),
-			},
-		)
+		kafka.Message{
+			Key:   []byte("Key-A"),
+			Value: []byte(payload),
+		},
+	)
+	
+	if err != nil {
+		log.Println("failed to write messages:", err)
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to send to Kafka"})
+		return
+	}
+	
+	// Return the result to the user
+	ctx.JSON(http.StatusCreated, "event sent")
+	
+})
 
-		if err != nil {
-			log.Println("failed to write messages:", err)
-			ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to send to Kafka"})
-			return
-		}
-
-		// 4. Return the result to the user
-		ctx.JSON(http.StatusCreated, "event sent")
-
-	})
-
-	r.Run()
+	r.Run(":8081")
 }
