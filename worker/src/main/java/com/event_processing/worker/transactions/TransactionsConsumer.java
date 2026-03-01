56c4a344eb213668ce04f9c329ecbb9269c13a39
Dillon <dfcross18@gmail.com>
2026-03-01 15:01:04 -0700

    added deserialization in springboot

--- worker/src/main/java/com/event_processing/worker/transactions/TransactionsConsumer.java
+++ worker/src/main/java/com/event_processing/worker/transactions/TransactionsConsumer.java
@@ -3,13 +3,27 @@ package com.event_processing.worker.transactions;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+import tools.jackson.databind.ObjectMapper;
 
 // listens to kafka, getting transaction events to be eventually stored in db
 @Component
 public class TransactionsConsumer {
 
+    private final ObjectMapper objectMapper;
+
+    public TransactionsConsumer(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
     @KafkaListener(id = "transaction-group", topics = "topic-A")
     public void listen(String message) {
-        System.out.println("Received message: " + message);
+        try {
+            System.out.println("Received message: " + message);
+
+            Transaction transaction = objectMapper.readValue(message, Transaction.class);
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 }
