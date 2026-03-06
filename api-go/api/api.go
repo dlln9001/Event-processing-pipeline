b245cdbbf6a6fb21ec1dc248ed4f18c90b0c8299
Dillon <dfcross18@gmail.com>
2026-03-06 13:35:07 -0700

    cleaned up kafka writer addresses

--- api-go/api/api.go
+++ api-go/api/api.go
@@ -18,9 +18,9 @@ func Run() {
 	fmt.Println("api.Run() running")
 
 	w := &kafka.Writer{
-		Addr:     kafka.TCP("localhost:9092", "localhost:9093", "localhost:9094"),
-		Topic:    "topic-A",
-		Balancer: &kafka.LeastBytes{},
+		Addr:         kafka.TCP("localhost:9092"),
+		Topic:        "topic-A",
+		Balancer:     &kafka.LeastBytes{},
 		Async:        false,
 		BatchTimeout: 8 * time.Millisecond,
 		WriteTimeout: 5 * time.Second,
@@ -29,46 +29,45 @@ func Run() {
 	defer w.Close()
 
 	r := gin.Default()
-	
+
 	r.GET("/ping", func(ctx *gin.Context) {
 		ctx.JSON(http.StatusOK, "hello")
 	})
-	
-	
-	// makes a transaction object and sends a transaction event to kafka 
+
+	// makes a transaction object and sends a transaction event to kafka
 	r.POST("/transaction", func(ctx *gin.Context) {
 		var req models.Transaction
 		err := ctx.ShouldBindJSON(&req)
-		
+
 		if err != nil {
 			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 			return
 		}
-		
+
 		payload, err := json.Marshal(req)
-		
+
 		if err != nil {
 			ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to encode JSON"})
 			return
 		}
-		
+
 		err = w.WriteMessages(context.Background(),
-		kafka.Message{
-			Key:   []byte("Key-A"),
-			Value: []byte(payload),
-		},
-	)
-	
-	if err != nil {
-		log.Println("failed to write messages:", err)
-		ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to send to Kafka"})
-		return
-	}
-	
-	// Return the result to the user
-	ctx.JSON(http.StatusCreated, "event sent")
-	
-})
+			kafka.Message{
+				Key:   []byte("Key-A"),
+				Value: []byte(payload),
+			},
+		)
+
+		if err != nil {
+			log.Println("failed to write messages:", err)
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to send to Kafka"})
+			return
+		}
+
+		// Return the result to the user
+		ctx.JSON(http.StatusCreated, "event sent")
+
+	})
 
 	r.Run(":8081")
 }
