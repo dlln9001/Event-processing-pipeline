c202df1f81813af109d839e462acd34c16df137a
Dillon <dfcross18@gmail.com>
2026-03-13 13:10:51 -0700

    added local terraform provisioning for aws EC2 instance

---
+++ infra/terraform/main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+  profile = "terraform-local"
+}
+
+# Automatically find the latest Amazon Linux AMI ID
+data "aws_ami" "latest_amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023*x86_64"]
+  }
+}
+
+# Create the EC2 Instance
+resource "aws_instance" "my_first_server" {
+  ami           = data.aws_ami.latest_amazon_linux.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "MyTerraformInstance"
+  }
+}
